1b5d363d0c52d278c40dca77033a40e9f38d0460
Diego Ramírez <diegosay777@gmail.com>
2023-04-26 17:32:14 -0600

    ajustes formulario de registro

--- views/customer/register.php
+++ views/customer/register.php
@@ -31,157 +31,161 @@
             </div>
             <br>
         </div>
-        <div class="container contenedorForm " id="contenedorCuenta">
-
-            <br>
-            <div class="form-outer ">
-                <div class="page">
-                    <form method="post" id="comprobar-form" autocomplete="off">
-                        <div class="form-group">
-                            <label for="txtCuenta">Ingresa tu número de cuenta</label>
-                            <input type="text" name="txtCuenta" id="txtCuenta" class="form-control input-forms" required>
-                        </div>
-                        <br>
-                        <a class="btn submit" href="../../index.php" id="btnInicio">Volver a inicio</a>
-                        <button type="submit" class="btn submit" id="btnComprobar">Comprobar</button>
-
-                    </form>
-                </div>
+        <div class="espacioForm">
+            <div class="container contenedorForm " id="contenedorCuenta">
                 <br>
+                <div class="form-outer ">
+                    <div class="page">
+                        <form method="post" id="comprobar-form" autocomplete="off">
+                            <div class="form-group">
+                                <label for="txtCuenta">Ingresa tu número de cuenta</label>
+                                <input type="text" name="txtCuenta" id="txtCuenta" class="form-control input-forms" required>
+                            </div>
+                            <br>
+                            <a class="btn submit" href="../../index.php" id="btnInicio">Volver a inicio</a>
+                            <button type="submit" class="btn submit" id="btnComprobar">Comprobar</button>
 
+                        </form>
+                    </div>
+                    <br>
+
+                </div>
             </div>
         </div>
+
         <br>
 
+        <div class="espacioForm">
+            <div class="container contenedorForm d-none" id="contenedorDatos">
+                <div class="form-outer ">
+                    <form method="post" id="register-form" autocomplete="off" class="formDatos">
+                        <div class="page">
+                            <div class="row">
+                                <div class="col-xl-6 col-md-6 col-sm-12 col-xs-12">
+                                    <div class="form-group">
+                                        <label for="txtNombre">Nombre</label>
+                                        <input type="text" name="txtNombre" id="txtNombre" class="form-control input-forms" required maxlength="50">
+                                    </div>
+                                    <br>
+                                    <div class="form-group">
+                                        <label for="txtApellido">Apellido</label>
+                                        <input type="text" name="txtApellido" id="txtApellido" class="form-control input-forms" required maxlength="50">
+                                    </div>
 
-        <div class="container contenedorForm d-none" id="contenedorDatos">
-            <div class="form-outer ">
-                <form method="post" id="register-form" autocomplete="off" class="formDatos">
-                    <div class="page">
-                        <div class="row">
-                            <div class="col-xl-6 col-md-6 col-sm-12 col-xs-12">
-                                <div class="form-group">
-                                    <label for="txtNombre">Nombre</label>
-                                    <input type="text" name="txtNombre" id="txtNombre" class="form-control input-forms" required maxlength="50">
-                                </div>
-                                <br>
-                                <div class="form-group">
-                                    <label for="txtApellido">Apellido</label>
-                                    <input type="text" name="txtApellido" id="txtApellido" class="form-control input-forms" required maxlength="50">
-                                </div>
-
-                                <br>
-                                <div class="form-group">
-                                    <label for="txtNacimiento">Fecha de nacimiento</label>
-                                    <input type="date" name="txtNacimiento" id="txtNacimiento" class="form-control input-forms" required>
-                                </div>
-                                <br>
-                                <div class="form-group">
-                                    <label for="txtOcupacion">Ocupación</label>
-                                    <input type="text" name="txtOcupacion" id="txtOcupacion" class="form-control input-forms" required maxlength="100">
-                                </div>
-                                <br>
-                                <div class="form-group">
-                                    <label for="txtDui">DUI</label>
-                                    <input type="text" name="txtDui" id="txtDui" class="form-control input-forms" required>
-                                </div>
-                                <br>
-                                <div class="form-group">
-                                    <label for="txtNum">Número de teléfono</label>
-                                    <input type="text" name="txtNum" id="txtNum" class="form-control input-forms" required>
-                                </div>
-                                <br>
-                                <label for="genero">Género</label>
-                                <select name="genero" id="genero" class="form-control input-forms" required>
-                                    <option value="">Seleccione</option>
-                                    <option value="M">Masculino</option>
-                                    <option value="F">Femenino</option>
-
-
-                                </select>
-                                <br>
-
-
-                                <input type="text" id="txtCuenta2" name="txtCuenta2" class="d-none">
-                            </div>
-                            <div class="col-xl-6 col-md-6 col-sm-12 col-xs-12">
-                                <div class="form-group">
-                                    <label for="estadoCivil">Estado civil</label>
-                                    <select name="estadoCivil" id="estadoCivil" class="form-control input-forms" required>
+                                    <br>
+                                    <div class="form-group">
+                                        <label for="txtNacimiento">Fecha de nacimiento</label>
+                                        <input type="date" name="txtNacimiento" id="txtNacimiento" class="form-control input-forms" required>
+                                    </div>
+                                    <br>
+                                    <div class="form-group">
+                                        <label for="txtOcupacion">Ocupación</label>
+                                        <input type="text" name="txtOcupacion" id="txtOcupacion" class="form-control input-forms" required maxlength="100">
+                                    </div>
+                                    <br>
+                                    <div class="form-group">
+                                        <label for="txtDui">DUI</label>
+                                        <input type="text" name="txtDui" id="txtDui" class="form-control input-forms" required>
+                                    </div>
+                                    <br>
+                                    <div class="form-group">
+                                        <label for="txtNum">Número de teléfono</label>
+                                        <input type="text" name="txtNum" id="txtNum" class="form-control input-forms" required>
+                                    </div>
+                                    <br>
+                                    <label for="genero">Género</label>
+                                    <select name="genero" id="genero" class="form-control input-forms" required>
                                         <option value="">Seleccione</option>
-                                        <option value="Casado">Casado/a</option>
-                                        <option value="Soltero">Soltero/a</option>
-                                        <option value="Viudo">Viudo/a</option>
+                                        <option value="M">Masculino</option>
+                                        <option value="F">Femenino</option>
 
 
                                     </select>
-                                </div>
-                                <br>
-                                <div class="form-group">
-                                    <label for="txtEmail">Correo electrónico</label>
-                                    <input type="email" name="txtEmail" id="txtEmail" class="form-control input-forms" required maxlength="50">
+                                    <br>
 
-                                </div>
-                                <br>
-                                <div class="form-group">
-                                    <label for="txtDireccion">Dirección</label>
-                                    <textarea type="text" name="txtDireccion" id="txtDireccion" class="form-control input-forms" required rows="1"></textarea>
-                                </div>
-                                <br>
-                                <div class="form-group">
-                                    <label for="username">Nombre de usuario</label>
-                                    <input type="text" name="username" id="username" class="form-control input-forms" required maxlength="50">
 
+                                    <input type="text" id="txtCuenta2" name="txtCuenta2" class="d-none">
                                 </div>
+                                <div class="col-xl-6 col-md-6 col-sm-12 col-xs-12">
+                                    <div class="form-group">
+                                        <label for="estadoCivil">Estado civil</label>
+                                        <select name="estadoCivil" id="estadoCivil" class="form-control input-forms" required>
+                                            <option value="">Seleccione</option>
+                                            <option value="Casado">Casado/a</option>
+                                            <option value="Soltero">Soltero/a</option>
+                                            <option value="Viudo">Viudo/a</option>
 
-                                <br>
 
-                                <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                                    <strong class="alertaText">Advertencia</strong>
-                                    <p class="alertaText">Tu contraseña debe contener al menos una letra mayúscula, una letra minúscula,
-                                        un número y un carácter especial, y que tenga entre 8 y 16 caracteres.
-                                        También asegúrate que la contraseña no contenga espacios en blanco</p>
-                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                                </div>
-                                <div style="position: relative;">
-                                    <label for="password">Contraseña</label>
-                                    <div style="position: relative;">
-                                        <input id="password" name="password" type="password" class="form-control input-forms"  required maxlength="16">
-                                        <button type="button" class="btnPass" onclick="togglePassword('password', 'Pass1')"><span id="Pass1" class="material-symbols-outlined">
-                                                visibility
-                                            </span></button>
+                                        </select>
                                     </div>
-                                </div>
+                                    <br>
+                                    <div class="form-group">
+                                        <label for="txtEmail">Correo electrónico</label>
+                                        <input type="email" name="txtEmail" id="txtEmail" class="form-control input-forms" required maxlength="50">
 
-                                <br>
-
-                                <div style="position: relative;">
-                                    <label for="password">Comprobar contraseña</label>
-                                    <div style="position: relative;">
-                                        <input id="passwordVerify" name="passwordVerify" type="password" class="form-control input-forms" required maxlength="16">
-                                        <button type="button" class="btnPass" onclick="togglePassword('passwordVerify', 'Pass2')"><span id="Pass2" class="material-symbols-outlined">
-                                                visibility
-                                            </span></button>
                                     </div>
+                                    <br>
+                                    <div class="form-group">
+                                        <label for="txtDireccion">Dirección</label>
+                                        <textarea type="text" name="txtDireccion" id="txtDireccion" class="form-control input-forms" required rows="1"></textarea>
+                                    </div>
+                                    <br>
+                                    <div class="form-group">
+                                        <label for="username">Nombre de usuario</label>
+                                        <input type="text" name="username" id="username" class="form-control input-forms" required maxlength="50">
+
+                                    </div>
+
+                                    <br>
+
+                                    <div class="alert alert-warning alert-dismissible fade show" role="alert" style="color:black !important;">
+                                        <strong class="alertaText">Advertencia</strong>
+                                        <p class="alertaText">Tu contraseña debe contener al menos una letra mayúscula, una letra minúscula,
+                                            un número y un carácter especial, y que tenga entre 8 y 16 caracteres.
+                                            También asegúrate que la contraseña no contenga espacios en blanco</p>
+                                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                    </div>
+                                    <div style="position: relative;">
+                                        <label for="password">Contraseña</label>
+                                        <div style="position: relative;">
+                                            <input id="password" name="password" type="password" class="form-control input-forms" required maxlength="16">
+                                            <button type="button" style="padding-top: 10px !important;" class="btnPass " onclick="togglePassword('password', 'Pass1')"><span id="Pass1" class="material-symbols-outlined eye">
+                                                    visibility
+                                                </span></button>
+                                        </div>
+                                    </div>
+
+                                    <br>
+
+                                    <div style="position: relative;">
+                                        <label for="password">Comprobar contraseña</label>
+                                        <div style="position: relative;">
+                                            <input id="passwordVerify" name="passwordVerify" type="password" class="form-control input-forms" required maxlength="16">
+                                            <button type="button" style="padding-top: 10px !important;" class="btnPass " onclick="togglePassword('passwordVerify', 'Pass2')"><span id="Pass2" class="material-symbols-outlined eye">
+                                                    visibility
+                                                </span></button>
+                                        </div>
+                                    </div>
+
+
+                                    <br>
+
+
+
+
+
                                 </div>
-
-
-                                <br>
-
-
-
-
 
                             </div>
 
                         </div>
-
-                    </div>
-                    <a class="btn submit" id="btnVolver">Volver</a>
-                    <button type="submit" class="btn submit">Crear cuenta</button>
-                </form>
+                        <a class="btn submit" id="btnVolver">Volver</a>
+                        <button type="submit" class="btn submit">Crear cuenta</button>
+                    </form>
+                </div>
             </div>
         </div>
+
 
 
 
@@ -190,7 +194,7 @@
 
 
 
-<!--     <button onclick="modoOscuro()">osc</button>
+    <!--     <button onclick="modoOscuro()">osc</button>
     <button onclick="modoClaro()">claro</button> -->
 
 
